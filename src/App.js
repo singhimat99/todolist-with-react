72fa3e892954aeb1b06bafcb89f4e5992b68ea71
Himat Singh <singhimat99@gmail.com>
2022-08-11 16:03:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useRef } from "react";
+import ToDoList from "./ToDoList";
+// import uuidv4 from "uuid/v4";
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    console.log(name);
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        { id: Math.random() * 100, name: name, complete: false },
+      ];
+    });
+    console.log(todos);
+    todoNameRef.current.value = null;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToDoList todos={todos} />
+      <input ref={todoNameRef} type="text"></input>
+      <button onClick={handleAddTodo}>Add ToDo</button>
+      <button>Clear</button>
+      <div>0 left</div>
+    </>
   );
 }
 
